d1f1f45e0b11640c41790a35a2947bb04e97e04c
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2026-07-09 21:50:43 +0600

    initial commit

--- bill_management/settings.py
+++ bill_management/settings.py
@@ -51,11 +51,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bill_management.wsgi.application'
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
